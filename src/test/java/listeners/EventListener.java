c8282127ab65a31b495276d8f2c90f6973808ca2
Anton Karpenko <toshidono.it.work@gmail.com>
2018-07-07 21:02:22 +0300

    Add logging

---
+++ src/test/java/listeners/EventListener.java
@@ -0,0 +1,127 @@
+package listeners;
+
+import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.events.WebDriverEventListener;
+
+public class EventListener implements WebDriverEventListener {
+
+    private Logger logger = Logger.getLogger(EventListener.class);
+
+    @Override
+    public void beforeAlertAccept(WebDriver webDriver) {
+
+    }
+
+    @Override
+    public void afterAlertAccept(WebDriver webDriver) {
+
+    }
+
+    @Override
+    public void afterAlertDismiss(WebDriver webDriver) {
+
+    }
+
+    @Override
+    public void beforeAlertDismiss(WebDriver webDriver) {
+
+    }
+
+    @Override
+    public void beforeNavigateTo(String s, WebDriver webDriver) {
+        logger.info("[Debug_AK] :: " + "beforeNavigateTo :: " + s);
+    }
+
+    @Override
+    public void afterNavigateTo(String s, WebDriver webDriver) {
+
+    }
+
+    @Override
+    public void beforeNavigateBack(WebDriver webDriver) {
+
+    }
+
+    @Override
+    public void afterNavigateBack(WebDriver webDriver) {
+
+    }
+
+    @Override
+    public void beforeNavigateForward(WebDriver webDriver) {
+
+    }
+
+    @Override
+    public void afterNavigateForward(WebDriver webDriver) {
+
+    }
+
+    @Override
+    public void beforeNavigateRefresh(WebDriver webDriver) {
+
+    }
+
+    @Override
+    public void afterNavigateRefresh(WebDriver webDriver) {
+
+    }
+
+    @Override
+    public void beforeFindBy(By by, WebElement webElement, WebDriver webDriver) {
+
+    }
+
+    @Override
+    public void afterFindBy(By by, WebElement webElement, WebDriver webDriver) {
+        logger.info("[Debug_AK] :: " + "afterFindBy :: by :: " + by);
+    }
+
+    @Override
+    public void beforeClickOn(WebElement webElement, WebDriver webDriver) {
+
+    }
+
+    @Override
+    public void afterClickOn(WebElement webElement, WebDriver webDriver) {
+
+    }
+
+    @Override
+    public void beforeChangeValueOf(WebElement webElement, WebDriver webDriver, CharSequence[] charSequences) {
+
+    }
+
+    @Override
+    public void afterChangeValueOf(WebElement webElement, WebDriver webDriver, CharSequence[] charSequences) {
+
+    }
+
+    @Override
+    public void beforeScript(String s, WebDriver webDriver) {
+
+    }
+
+    @Override
+    public void afterScript(String s, WebDriver webDriver) {
+
+    }
+
+    @Override
+    public void beforeSwitchToWindow(String s, WebDriver webDriver) {
+
+    }
+
+    @Override
+    public void afterSwitchToWindow(String s, WebDriver webDriver) {
+
+    }
+
+    @Override
+    public void onException(Throwable throwable, WebDriver webDriver) {
+
+    }
+}
--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,6 +1,15 @@
 package tests;
 
+import listeners.EventListener;
+import listeners.TestListener;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.logging.LogEntries;
+import org.openqa.selenium.logging.LogEntry;
+import org.openqa.selenium.logging.LogType;
+import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
 import utils.Driver;
@@ -8,7 +17,9 @@ import utils.PropertyReader;
 import utils.helpers.WaitingsHelpers;
 
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
 
+@Listeners(TestListener.class)
 public class BaseTest {
 
     public int baseTimeout = Integer.parseInt(PropertyReader.
@@ -22,6 +33,18 @@ public class BaseTest {
         driver = Driver.getInstance();
         driver.manage().timeouts().implicitlyWait(baseTimeout, TimeUnit.SECONDS);
         webDriverWait = WaitingsHelpers.getInstanceWebDriverWait();
+//
+//        //            ChromeOptions options = new ChromeOptions();
+//        DesiredCapabilities caps = DesiredCapabilities.chrome();
+//        LoggingPreferences logs = new LoggingPreferences();
+//        logs.enable(LogType.PERFORMANCE, Level.INFO);
+//        caps.setCapability(CapabilityType.LOGGING_PREFS, logs);
+//
+//        driver = new ChromeDriver(caps);
+//        LogEntries logEntries = driver.manage().logs().get(LogType.PERFORMANCE);
+//        for (LogEntry entry : logEntries){
+//            System.out.println(entry.getLevel() + " " + entry.getMessage());
+//        }
     }
 
     @AfterSuite
